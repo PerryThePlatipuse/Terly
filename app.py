6d06ea56be9b32580bcec2a1f9950727559b5c2d
Alex Shcherbakov <shcherbakov.032005@gmail.com>
2021-12-20 10:18:31 +0300

    kal comit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-
+print("kal")
 @app.route('/')
 def hello_world():
     print("yes")# put application's code here

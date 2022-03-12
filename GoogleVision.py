f26a9c06e613852ab2310d2d8d7eb4b666f5074a
Алексей <shcherbakov.032005@gmail.com>
2022-03-12 13:54:50 +0300

    added imgs

--- GoogleVision.py
+++ GoogleVision.py
@@ -1,10 +1,11 @@
 import requests
 import json
 import os
-from PIL import Image
 import time
+import numpy as np
+import cv2
+from private_key import private_secret_key
 
-private_secret_key = "AIzaSyDEN_6QPOSdEES8AjkfUuWg3QShB0bvKA8"
 
 url = "https://vision.googleapis.com/v1/images:annotate"
 
@@ -12,6 +13,80 @@ querystring = {"key": private_secret_key}
 headers = {
     'Content-Type': "application/json",
 }
+
+
+def encode_text(recognised, text):
+    with open("encoded_text.terly", 'w') as f:
+        f.write(str(int(recognised)) + text)
+
+
+def decode_text():
+    with open("encoded_text.terly", 'r') as f:
+        ans = f.read().strip()
+        return bool(int(ans[0])), ans[1:]
+
+
+encode_text(False, 'Ошибка распознавания, пожалуйста, повторите позднее')
+
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
 
 
 def image_request():
@@ -45,12 +120,31 @@ def image_request():
             }
         ]
     }
-    json.dump(payload, open("request.json", 'w'))
-    img = Image.open("static/styles/images/terly_photo.png")
-    x, y = img.size
-    response = requests.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring)
-    parsed_text = []
-    for i in response.json()["responses"][0]["textAnnotations"][1:]:
-        coords = i["boundingPoly"]["vertices"]
-        # croping imgs
-    return response.json()["responses"][0]["fullTextAnnotation"]["text"].replace('\n', ' ')
+    try:
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring)
+        with open("database/how_much.txt", 'r') as f:
+            count = int(f.read().strip())
+        with open("database/annotations.json", 'r') as f:
+            dct = json.load(f)
+        with open("database/annotations.json", 'w') as f:
+            for i in response.json()["responses"][0]["textAnnotations"][1:]:
+                count += 1
+                coords = i["boundingPoly"]["vertices"]
+                description = i["description"]
+                dct[count] = description
+                pct = four_point_transform(cv2.imread("static/styles/images/terly_photo.png"),
+                                           np.array([[coords[0]["x"], coords[0]["y"]],
+                                                     [coords[1]["x"], coords[1]["y"]],
+                                                     [coords[2]["x"], coords[2]["y"]],
+                                                     [coords[3]["x"], coords[3]["y"]]]))
+                cv2.imwrite(f"database/db/file{count}.png", pct)
+            json.dump(dct, f, ensure_ascii=False)
+            text = response.json()["responses"][0]["fullTextAnnotation"]["text"].replace('\n', ' ')
+        with open("database/how_much.txt", 'w') as f:
+            f.write(str(count))
+    except:
+        text = "Нестабильное подключение к интернету, пожалуйста, переключитесь на другую сеть"
+    encode_text(True, text)
+
+
+image_request()
--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import shutil
 import multiprocessing
-import requests
-import json
 import os
 import time
-from PIL import Image
-
-
-private_secret_key = "AIzaSyDEN_6QPOSdEES8AjkfUuWg3QShB0bvKA8"
-url = "https://vision.googleapis.com/v1/images:annotate"
-querystring = {"key": private_secret_key}
-headers = {'Content-Type': "application/json",}
+from GoogleVision import image_request, encode_text, decode_text
 
 
 app = Flask(__name__)
@@ -21,66 +13,6 @@ to_del = map(lambda x: os.path.join(path, x.name),
              filter(lambda x: "terly_photo" in x.name, os.scandir("/Users/alex/Downloads/")))
 for i in to_del:
     os.remove(i)
-
-
-def encode_text(recognised, text):
-    with open("encoded_text.terly", 'w') as f:
-        f.write(str(int(recognised)) + text)
-
-
-def decode_text():
-    with open("encoded_text.terly", 'r') as f:
-        ans = f.read().strip()
-        return bool(int(ans[0])), ans[1:]
-
-
-encode_text(False, 'Ошибка распознавания, пожалуйста, повторите позднее')
-
-
-def image_request():
-    os.system("base64 -i 'static/styles/images/terly_photo.png' -o encoded_img.terly")
-    time.sleep(0.5)
-    with open("encoded_img.terly") as f:
-        contet_ans = f.read().strip()
-    payload = {
-        "requests": [
-            {
-                "features": [
-                    {
-                        "maxResults": 50,
-                        "model": "builtin/latest",
-                        "type": "DOCUMENT_TEXT_DETECTION"
-                    }
-                ],
-                "image": {
-                    "content": str(contet_ans)
-                },
-                "imageContext": {
-                    "cropHintsParams": {
-                        "aspectRatios": [
-                            0.8,
-                            1,
-                            1.2
-                        ]
-                    },
-                    "languageHints": ["ru"]
-                }
-            }
-        ]
-    }
-    try:
-        json.dump(payload, open("request.json", 'w'))
-        img = Image.open("static/styles/images/terly_photo.png")
-        x, y = img.size
-        response = requests.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring)
-        parsed_text = []
-        for i in response.json()["responses"][0]["textAnnotations"][1:]:
-            coords = i["boundingPoly"]["vertices"]
-            # croping imgs
-        text = response.json()["responses"][0]["fullTextAnnotation"]["text"].replace('\n', ' ')
-    except:
-        text = "Нестабильное подключение к интернету, пожалуйста, переключитесь на другую сеть"
-    encode_text(True, text)
 
 
 @app.route('/')
